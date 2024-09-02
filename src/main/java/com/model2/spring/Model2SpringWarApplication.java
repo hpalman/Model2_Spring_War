ad9ec6a6635d62d175a9916b60c212990fe93aa3
H.PALMAN <hpalman@gmail.com>
2024-09-02 09:22:31 +0900

    첫

---
+++ src/main/java/com/model2/spring/Model2SpringWarApplication.java
@@ -0,0 +1,13 @@
+package com.model2.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model2SpringWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Model2SpringWarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/model2/spring/Model2SpringWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model2.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Model2SpringWarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
